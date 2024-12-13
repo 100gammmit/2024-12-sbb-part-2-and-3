fd706ed4753c3b04b9e3bcfbe47ad94da93014cf
100gammmit <100minha@naver.com>
2024-12-13 16:57:13 +0900

    점프투스프링 태초에서 재시작

---
+++ src/test/java/dev/spring/sbbpart2and3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.spring.sbbpart2and3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
